c2cab07d17aa3d3af21237f3d0dd9546c7254db0
Jsh <king601143@gmail.com>
2026-04-09 13:03:16 +0900

    refactor: align home route with pages structure

--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,13 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
-
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: () => import('@/pages/homePage/HomePage.vue'),
+    },
 
     {
       path: '/signup',
@@ -21,8 +25,7 @@ const router = createRouter({
       path: '/setting',
       name: 'setting',
       component: () => import('@/pages/settingPage/SettingPage.vue'),
-    }
-
+    },
   ],
 })
 
--- src/views/HomeView.vue
+++
@@ -1,7 +0,0 @@
-<script setup>
-import HomePage from '../pages/home.vue'
-</script>
-
-<template>
-  <HomePage />
-</template>
