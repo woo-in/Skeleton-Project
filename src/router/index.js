986091e5a876a9760ea359e5c1ce622b9c724d6a
hisemsem.github.io <121273265+hisemsem@users.noreply.github.com>
2026-04-09 11:36:07 +0900

    feat: 목표 주식 화면 신규 개발 (#3)

--- src/router/index.js
+++ src/router/index.js
@@ -1,20 +1,13 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue' // 곧 만들 파일입니다
+import Setup from '@/pages/Setup/ExpenseGoalSetup.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/',
-      name: 'home',
-      component: HomeView,
-    },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // 이 부분은 나중에 필요할 때 파일을 만들면 됩니다.
-      component: () => import('../views/AboutView.vue'),
+      path: '/setup',
+      name: 'setup',
+      component: Setup,
     },
   ],
 })
